ac3801045627d9fdfdc8a6aad3df5192177c9a06
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2018-05-24 13:28:00 -0500

    se organizó el build.gradle

--- proyecto1/src/main/java/co/ceiba/domain/Person.java
+++ proyecto1/src/main/java/co/ceiba/domain/Person.java
@@ -12,15 +12,5 @@ public class Person {
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
 	
 }
