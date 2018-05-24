091791c451c8d69b093817a48e8208576828c9fd
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2018-05-24 10:01:32 -0500

    primer commit

---
+++ proyecto1/src/main/java/co/ceiba/testdatabuilder/PersonTestDataBuilder.java
@@ -0,0 +1,28 @@
+package co.ceiba.testdatabuilder;
+
+import co.ceiba.domain.Person;
+
+public class PersonTestDataBuilder {
+	
+	private String name;
+	private String lastname;
+	
+	public PersonTestDataBuilder() {
+		this.name = "Ceiba";
+		this.lastname = "Software House";
+	}
+	
+	public PersonTestDataBuilder withName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public PersonTestDataBuilder whithLastName(String lastName) {
+		this.lastname = lastName;
+		return this;
+	}
+	
+	public Person build() {
+		return new Person(this.name, this.lastname);
+	}
+}
---
+++ proyecto1/src/test/java/co/ceiba/service/NitifyPersonServiceTest.java
@@ -0,0 +1,33 @@
+package co.ceiba.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+public class NitifyPersonServiceTest {
+	
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setup() {
+		emailService = Mockito.mock(EmailService.class);
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void notifyTest() {
+		//Arrange
+		Mockito.when(emailService.sendMail(Mockito.anyString())).thenReturn("Hola Mundo");
+		Person person = new PersonTestDataBuilder().build();
+		//Act
+		String message = notifyPersonService.notify(person);
+		//Assert
+		Assert.assertNotNull(message);
+		System.out.println(message);
+	}
+}
